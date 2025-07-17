32a43d573a7df5933d22dbabcd8d2b8092fa1006
nrsimonelli <nrsimonelli@gmail.com>
2025-07-16 20:05:33 -0500

    frontend

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-1'>
+        <h2 className='text-2xl font-bold text-gray-800'>UO Showdown</h2>
+        <p className='text-gray-600'>
+          A simple project with overengineered solutions
+        </p>
+      </div>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md'>
+        Lets go
+      </button>
+    </div>
+  )
+}
+
+export default App
